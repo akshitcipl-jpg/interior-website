b25f33aa86ca5a26a1ac2201ba171a6eb49841d8
Akshit Sharma <akshitsharma9193@gmail.com>
2026-09-24 16:37:59 +0530

    Fix GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -13,37 +13,24 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
 
       <main>
         <Routes>
-
           <Route path="/" element={<Home />} />
-
           <Route path="/about" element={<About />} />
-
           <Route path="/kitchen" element={<Kitchen />} />
-
           <Route path="/bedroom" element={<Bedroom />} />
-
           <Route path="/gallery" element={<Gallery />} />
-
-          <Route
-            path="/gallery/:id"
-            element={<GalleryDetails />}
-          />
-
+          <Route path="/gallery/:id" element={<GalleryDetails />} />
           <Route path="/contact" element={<Contact />} />
-
-          {/* Unknown URL */}
           <Route path="*" element={<Home />} />
-
         </Routes>
       </main>
 
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
